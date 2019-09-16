76653bb02c1cb6ea23ab9c9b40d24dbdcc60ef30
TheChickenNagget <54007132+TheChickenNagget@users.noreply.github.com>
2019-09-16 20:00:18 +0200

    mini update

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+module.exports = {
+/**
+* Detect if number / string is binary number
+* @param {string} text
+* @example
+* var test = require("isbinary-num")
+* console.log(test.isBinary("010100101001010010101");
+* // outputs "true"
+* @returns {boolean} boolean
+*/
+isBinary(t) { if (/^[01][01\s]*[01]$/.test(t)) { return true; } return false; },
+};
---
+++ src/testing.js
@@ -0,0 +1,2 @@
+console.log(require("./main").isBinary("00000000001"));
+console.log(require("./main").isBinary("testa"));
