641b9be8275ead50fd0cf0207d4fa12c83b81d29
TheChickenNagget <thechickennagget@gmail.com>
2020-12-29 23:47:18 +0100

    Enhance CLI

--- src/cli.js
+++ src/cli.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node 
-'use strict'; var a = process.argv.slice(2).toString();if(!/\S/.test(a)){return console.error("Text Cannot Be Empty Spaces")}console.log(require("./main.js")(a))
+'use strict';
+var a = process.argv.slice(2).toString();
+ if(!/\S/.test(a)){
+   return console.error("Text Cannot Be Empty Spaces")}
+  console.log(require("./main.js")(a))
